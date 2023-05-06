eb9bd127929a7593c8861116f9ff8ba94642d4cd
sh gdr <goudarzi.sharzad825@gmail.com>
2023-05-06 10:24:47 +0330

    add paths to urls.py index, login, register, logout

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from account import views
+from account import views as account_view
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', account_view.index, name='index'),
+    path('login/', account_view.user_login, name='login'),
+    path('register/', account_view.user_register, name='register'),
+    path('logout/', account_view.user_logout, name='logout'),
 ]
