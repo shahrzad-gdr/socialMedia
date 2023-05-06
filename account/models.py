2f0458d013695d1439881ba9305fec2584e21bac
sh gdr <goudarzi.sharzad825@gmail.com>
2023-05-06 10:23:36 +0330

    add CustomUser and Profile models models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.db.models.signals import post_save
+from socialMedia import settings
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(verbose_name='email address', unique=True)
+    objects = UserManager()
+
+    REQUIRED_FIELDS = []
+
+
+class Profile(models.Model):
+    user  = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(default='profile_icon.png', upload_to='media/profile_images/')
+
+    def __str__(self):
+        return f"{self.user}"
+
+
+def save_profile(sender,**kwargs):
+    if kwargs['created']:
+        profile = Profile(user=kwargs['instance'])
+        profile.save()
+
+post_save.connect(save_profile, sender=CustomUser)
+
